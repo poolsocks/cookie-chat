2ac7bd5cf0800d83e2529bc3a608148f03ba18bb
Peter Kincaid <loaatao@gmail.com>
2017-09-06 14:20:02 -0500

    By far the coolest part so far

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,22 +1,26 @@
-angular.module('chatroom').controller('mainCtrl', function($scope, messageService){
+angular
+  .module("chatroom")
+  .controller("mainCtrl", function($scope, messageService) {
+    $scope.getMessages = function() {
+      messageService.getMessages().then(function(response) {
+        console.log(response);
+        $scope.messages = response.data;
+      });
+    };
+    $scope.postMessage = function(message) {
+      messageService.postMessage(message).then(function(response) {
+        $scope.messages = response.data;
+      });
+    };
 
-  //The getMessages function will call the getMessages method on the messageService object.
-  //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
-
-
-
-
-  //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
-  //pass that text to the postMessage method on the messageService object which will
-  //then post it to the backend.
-
-
-
-  //uncomment this code when your getMessages function is finished
-  //This goes and gets new data every second, which mimicking a chat room experience.
-  //  setInterval(function(){
-    // $scope.getMessages();
-
-  // }, 1500)
-
-})
+    //The getMessages function will call the getMessages method on the messageService object.
+    //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
+    //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
+    //pass that text to the postMessage method on the messageService object which will
+    //then post it to the backend.
+    //uncomment this code when your getMessages function is finished
+    //This goes and gets new data every second, which mimicking a chat room experience.
+    setInterval(function() {
+      $scope.getMessages();
+    }, 1500);
+  });
